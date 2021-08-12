e80d774399face71d13f834d6267dbfb45fbeeff
omkargb <omkargb107@gmail.com>
2021-08-12 20:29:21 +0530

    [omkargb] Add : User's mobile number validation implemented.

--- UserRegistrationProgram/InputValidation.cs
+++ UserRegistrationProgram/InputValidation.cs
@@ -9,22 +9,34 @@ namespace UserRegistration
     {
         public void UserInput()
         {
-            
-            const string Regex_EmailId = "^[A-Za-z0-9]+([.+-_][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][A-Za-z]+([.][A-Za-z]+)?$";
+            const string Regex_MobileNum = "^[1-9]{1,2}[ ][1-9]{1}[0-9]{9}$";
+            Console.Write("\n Enter your mobile number (CountryCode 10DigitNumber) : ");
+            string mobile = Console.ReadLine();
 
-            Console.Write(" Enter your email-Id : ");
-            string email = Console.ReadLine();
-
-            if (Regex.IsMatch(email, Regex_EmailId) == true)
+            if (Regex.IsMatch(mobile, Regex_MobileNum) == true)
             {
-                Console.WriteLine(" Email Id - Valid");
+                Console.WriteLine(" Mobile number is Valid. ");
             }
             else
             {
-                Console.WriteLine(" Email Id - Not valid");
+                Console.WriteLine(" Mobile number is Not Valid. ");
                 UserInput();
             }
 
+            //const string Regex_EmailId = "^[A-Za-z0-9]+([.+-_][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][A-Za-z]+([.][A-Za-z]+)?$";
+
+            //Console.Write(" Enter your email-Id : ");
+            //string email = Console.ReadLine();
+
+            //if (Regex.IsMatch(email, Regex_EmailId) == true)
+            //{
+            //    Console.WriteLine(" Email Id - Valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Email Id - Not valid");
+            //    UserInput();
+            //}
             //const string Regex_Name = "^[A-Z]{1}[A-Za-z]{2,}";
             //Console.WriteLine("\n Last name : Starts with Capital alphabet and has minimum 3 characters..");
             //Console.Write(" Enter Last name : ");
