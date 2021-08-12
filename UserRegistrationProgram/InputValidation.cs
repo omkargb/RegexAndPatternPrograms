f39014c9029ce25bb197cd5ecd3b43491b0e3541
omkargb <omkargb107@gmail.com>
2021-08-12 19:41:54 +0530

    [omkargb] Add : User's email-Id validation implemented.

--- UserRegistrationProgram/InputValidation.cs
+++ UserRegistrationProgram/InputValidation.cs
@@ -9,21 +9,36 @@ namespace UserRegistration
     {
         public void UserInput()
         {
-            Console.WriteLine("\n Last name : Starts with Capital alphabet and has minimum 3 characters..");
-            const string Regex_Name = "^[A-Z]{1}[A-Za-z]{2,}";
+            
+            const string Regex_EmailId = "^[A-Za-z0-9]+([.+-_][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][A-Za-z]+([.][A-Za-z]+)?$";
 
-            Console.Write(" Enter Last name : ");
-            //string firstName = Console.ReadLine();
-            string lastName = Console.ReadLine();
-            if (Regex.IsMatch(lastName, Regex_Name) == true)
+            Console.Write(" Enter your email-Id : ");
+            string email = Console.ReadLine();
+
+            if (Regex.IsMatch(email, Regex_EmailId) == true)
             {
-                Console.WriteLine(" Last name - Valid");
+                Console.WriteLine(" Email Id - Valid");
             }
             else
             {
-                Console.WriteLine(" Last name - Not valid");
+                Console.WriteLine(" Email Id - Not valid");
                 UserInput();
             }
+
+            //const string Regex_Name = "^[A-Z]{1}[A-Za-z]{2,}";
+            //Console.WriteLine("\n Last name : Starts with Capital alphabet and has minimum 3 characters..");
+            //Console.Write(" Enter Last name : ");
+            //string firstName = Console.ReadLine();
+            //string lastName = Console.ReadLine();
+            //if (Regex.IsMatch(lastName, Regex_Name) == true)
+            //{
+            //    Console.WriteLine(" Last name - Valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Last name - Not valid");
+            //    UserInput();
+            //}
         }
     }
 }
