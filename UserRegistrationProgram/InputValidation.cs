f97da8b36c1c6da19d3d95f5340711a6e8e38ab6
omkargb <omkargb107@gmail.com>
2021-08-13 00:13:37 +0530

    [omkargb] Add : Password validation Rules - min 8 characters, Atleast 1 Uppercase letter, Atleast 1 number, 1 special char - implemented.

--- UserRegistrationProgram/InputValidation.cs
+++ UserRegistrationProgram/InputValidation.cs
@@ -9,10 +9,11 @@ namespace UserRegistration
     {
         public void UserInput()
         {
-            const string Regex_Password = "^(?=.*[a-z0-9A-Z])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+                                     
+            const string Regex_Password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%^&*_+=,./?]).{8,}$";
             //(?=.*[A-Z]) - atleast 1 capital letter
             //(?=.*[0-9]) - atleast 1 number
-            Console.WriteLine("\n Password Rules --> 1]Minimum 8 Characters\t2]At least 1 capital alphabet\t3]At least 1 number");
+            Console.WriteLine("\n Password Rules --> 1]Minimum 8 Characters\t2]At least 1 capital alphabet\t3]At least 1 number\t4]Special char");
             Console.Write("\n Enter password : ");
             string password = Console.ReadLine();
 
