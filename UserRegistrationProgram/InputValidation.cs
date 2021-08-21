51be03d8ccb991e77f7ae930c4974f008f19f58d
omkargb <omkargb107@gmail.com>
2021-08-21 23:26:39 +0530

    [omkargb] Add : Using lambda function for validation of user details - implemented

--- UserRegistrationProgram/InputValidation.cs
+++ UserRegistrationProgram/InputValidation.cs
@@ -7,103 +7,22 @@ namespace UserRegistration
 {
     public class InputValidation
     {
-        public void UserInput()
-        { 
-        }
-
         const string Regex_Name = "^[A-Z]{1}[A-Za-z]{2,}";
         const string Regex_MobileNum = "^[1-9]{1,2}[ ][1-9]{1}[0-9]{9}$";
         const string Regex_Password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%^&*_+=,./?]).{8,}$";
         const string Regex_EmailId = "^[A-Za-z0-9]+([.+-_][A-Za-z0-9]+)*[@][a-zA-Z0-9]+([.][A-Za-z]{2,})+([.][A-Za-z]+)?$";
 
-        public bool TestFirstName(string firstName)
-        {
-            try
-            {
-                if (Regex.IsMatch(firstName, Regex_Name))
-                { return true; }
-                else
-                {
-                    throw new InvalidDetailsException(InvalidDetailsException.ExceptionType.FIRSTNAME_INVALID, "First_Name is Invalid.");
-                }
-            }
-            catch (InvalidDetailsException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
-        public bool TestLastName(string lastName)
-        {
-            try
-            {
-                if (Regex.IsMatch(lastName, Regex_Name))
-                { return true; }
-                else
-                {
-                    throw new InvalidDetailsException(InvalidDetailsException.ExceptionType.LASTNAME_INVALID, "Last_Name is Invalid.");
-                }
-            }
-            catch (InvalidDetailsException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
+        //using lambda function
+        public bool TestFirstName(string firstName) => (Regex.IsMatch(firstName, Regex_Name));
+        
+        public bool TestLastName(string lastName) => (Regex.IsMatch(lastName, Regex_Name));
+        
+        public bool TestMobileNum(string mobileNum) => (Regex.IsMatch(mobileNum, Regex_MobileNum));
+        
+        public bool TestPassword(string password) => (Regex.IsMatch(password, Regex_Password));
+        
+        public bool TestEmailId(string emailId) => (Regex.IsMatch(emailId, Regex_EmailId));
 
-        public bool TestMobileNum(string mobileNum)
-        {
-            try
-            {
-                if (Regex.IsMatch(mobileNum, Regex_MobileNum))
-                { return true; }
-                else
-                {
-                    throw new InvalidDetailsException(InvalidDetailsException.ExceptionType.MOBILENUMBER_INVALID, "Mobile Number is Invalid.");
-                }
-            }
-            catch (InvalidDetailsException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
-
-        public bool TestPassword(string password)
-        {
-            try
-            {
-                if (Regex.IsMatch(password, Regex_Password))
-                { return true; }
-                else
-                {
-                    throw new InvalidDetailsException(InvalidDetailsException.ExceptionType.PASSWORD_INVALID, "Password is Invalid.");
-                }
-            }
-            catch (InvalidDetailsException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
-
-        public bool TestEmailId(string emailId)
-        {
-            try
-            {
-                if (Regex.IsMatch(emailId, Regex_EmailId))
-                { return true; }
-                else
-                {
-                    throw new InvalidDetailsException(InvalidDetailsException.ExceptionType.EMAILID_INVALID, "Email-Id is Invalid.");
-                }
-            }
-            catch (InvalidDetailsException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
 
     }
 }
--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -9,7 +9,6 @@ namespace UserRegistration
             Console.WriteLine(" Welcome to User Registration Program and Input validation using RegEx \n");
 
             //InputValidation validate = new InputValidation();
-            //validate.UserInput();
         }
     }
 }
